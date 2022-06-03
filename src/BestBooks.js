79351e1380c6a216aaf5963c9e15a8c8bc78de3c
Natalija Germek <n.germek@icloud.com>
2022-06-02 21:51:26 -0700

    added components and code for About.js, app.js, bestBooks.js

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Image from 'react-bootstrap/Image';
+import bookImg from './libraryImage.jpg';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -10,6 +13,26 @@ class BestBooks extends React.Component {
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
+  componentDidMount = async () => {
+    try {
+      const config = {
+        method: 'get',
+        baseURL: process.env.REACT_APP_SERVER,
+        url: './books'
+      }
+
+      const response = await axios(config);
+      this.setState({
+        books: response.data
+      })
+    } catch (error) {
+      console.error('Error in BestBooks componentDidMount', error);
+      this.setState({
+        errorMessage: `Status Code: ${error.response.status}: ${error.response.data}`
+      })
+    }
+  }
+
   render() {
 
     /* TODO: render all the books in a Carousel */
@@ -19,7 +42,23 @@ class BestBooks extends React.Component {
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+          <Carousel>
+            {this.state.books.map(book => (   
+              <Carousel.Item>
+            <Image
+              className="w-100"
+              src={bookImg}
+              alt={book.title}
+              />
+            <Carousel.Caption>
+              <h2>First slide label</h2>
+              <p className="carousel-text">{book.title}</p>
+              <p className="carousel-text">{book.description}</p>
+              <p className="carousel-text">Status: {book.status}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+              ))}
+          </Carousel>
         ) : (
           <h3>No Books Found :(</h3>
         )}
